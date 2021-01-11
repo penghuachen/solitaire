866955b0af40dfda861d2f4871703795645d841d
Penghua <kids5346@gmail.com>
2021-01-11 15:42:09 +0800

    refactor: opening component

--- js/index.js
+++ js/index.js
@@ -1,32 +1,10 @@
-const UIStates = {
-  opening: true
-};
 
 (function() {
-  mainRenderProccess();
   setTimeout(hideOpening, 5000)
 })();
 
 function hideOpening() {
-  UIStates.opening = false;
-  mainRenderProccess();
+  document.querySelector(".opening").style = "display: none";
 }
 
-function openingDOM() {
-  const dom = `
-    <div class="opening">
-      <div class="opening-logo">
-        <img src="./images/logo.svg" alt="logo">
-      </div>
-    </div>
-  `;
 
-  return UIStates.opening ? dom : "";
-}
-
-function mainRenderProccess() {
-  const opening = openingDOM();
-  const mainDOM = opening
-
-  document.body.innerHTML = mainDOM;
-}
